7520d2c8944afacc6376ac25e86a0fd4b56f9d20
Caique Alves Andrade Oliveira <55563143+Caique-Andrade@users.noreply.github.com>
2025-08-08 08:14:09 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const botao = document.querySelector(".btn-comprar");
+    botao.addEventListener("click", () => {
+        window.open("https://go.hotmart.com/V101226787W", "_blank");
+    });
+});
